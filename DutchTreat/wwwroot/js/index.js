5ef67233fda78b73b5a1c3fe3ced0d3def46f29b
Brigitta Nicoara <brigi.brigitte@gmail.com>
2019-12-05 16:58:38 +0200

    Issue #4: - Adding JavaScript file - Adding jQuery modules, and Bootstrap module

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,21 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Hello Pluralsight");
+
+
+jQuery 
+
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var buyButton = document.getElementById("buyButton");
+buyButton.addEventListener("click", function () {
+    console.log("buying Item");
+})
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
+//functions foo()
+
