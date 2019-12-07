4b52fbbe87cc3fd856f8bfd069b83bbe9c1eeb29
Brigitta Nicoara <brigi.brigitte@gmail.com>
2019-12-07 20:39:30 +0200

    Issue #4: - customize the page

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,21 +1,30 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
 
-console.log("Hello Pluralsight");
-
-
-jQuery 
+    console.log("Hello Pluralsight");
 
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
 
-var buyButton = document.getElementById("buyButton");
-buyButton.addEventListener("click", function () {
-    console.log("buying Item");
-})
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].children;
-//functions foo()
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("buying Item");
+    })
 
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    })
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(1000);
+    })
+
+
+});
