2b5344334fc0d28b0bc3c67be1d057dcc282ff11
Brigitta Nicoara <brigi.brigitte@gmail.com>
2020-04-13 18:33:45 +0300

    - validations - add service for sending e-mail

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,13 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             //throw new InvalidOperationException();
@@ -18,8 +27,21 @@ namespace DutchTreat.Controllers
         [HttpGet("contact")]
         public IActionResult Contact()
         {
-            ViewBag.Title = "Contact Us";
+            
+            //throw new InvalidOperationException("Bad things happen");
+            return View();
+        }
 
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("brigitte.antal@yahoo.com", model.Subject, $"From: {model.Name} - {model.Email}, Message:{model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            
             return View();
         }
 
