2e0c1c02ec5914185cbbe421a76cb97754f2ad0e
Brigitta Nicoara <brigi.brigitte@gmail.com>
2020-04-15 12:51:53 +0300

    Create the project

---
+++ GradesBookProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GradesBookProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
