cc0a557f9b151fc400d590ea7ea8b9160fd0ff41
Brigitta Nicoara <brigi.brigitte@gmail.com>
2021-01-22 13:37:00 +0200

    Creation of HospitalAdministration project

---
+++ HospitalAdministration/Data/Entity/Doctor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalAdministration.Data.Entity
+{
+
+    public abstract class Doctor
+    {
+
+        public int Id { get; set; }
+        
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public long CNP { get; set; }
+        public int Age { get; set; }
+        public DateTime HireDate { get; set; }
+        public string UniversityName { get; set; }
+        public int ResidencyYers { get; set; }
+        public int ResidencyScor { get; set; }
+        public abstract float RiscFactor { get; }
+        public float Salary
+        {
+            get
+            {
+                return CalculateSalary();
+            }
+        }
+
+        public abstract Doctor AddDoctor(Doctor doctor);
+        public abstract List<Doctor> GetDoctors();
+        public abstract Doctor EditDoctor(Doctor doctor);
+        public abstract void DeleteDoctor(Doctor doctor);
+
+        private float CalculateSalary()
+        {
+            return 1500 * RiscFactor * (DateTime.Now.Year - HireDate.Year);
+        }
+    }
+}
---
+++ HospitalAdministration/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalAdministration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Introduceti CNP");
+            string cnp = Console.ReadLine();
+
+            string yearString = cnp.Substring(1, 2);
+            Console.WriteLine("Anul extras din cnp: "+ yearString);
+            List<char> numbers = new List<char>() { '1', '2', '0' };
+
+            string yearComplete = (yearString.StartsWith('0') || yearString.StartsWith('1') || yearString.StartsWith('2')) 
+                ? "20" + yearString : "19" + yearString;
+
+            Console.WriteLine("Anul complet extras din cnp: "+ yearComplete);
+
+            string monthString = cnp.Substring(3, 2);
+            string dayString = cnp.Substring(5, 2);
+
+            DateTime birtDate = Convert.ToDateTime( dayString + "/" + monthString + "/" + yearComplete);
+
+            Console.WriteLine("Data nasterii: "+ birtDate);
+            Console.WriteLine("Data de azi: "+ DateTime.Now);
+
+            DateTime age = DateTime.MinValue + (DateTime.Now - birtDate);
+
+            Console.WriteLine(@"Ai {0} ani, {1} luni, {2} zile", age.Year-1, age.Month-1, age.Day-1);
+
+        }
+
+    }
+}
